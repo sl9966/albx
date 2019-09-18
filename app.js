5e4f1a0cf7b510afe81fcbcad5079e286cfd890f
ShengLiang <729678028@qq.com>
2019-09-18 18:44:34 +0800

    02-搭建服务器-静态资源托管-设置ejs

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//监听端口
+app.listen(3382,()=>{
+  console.log('http://127.0.0.1:3382');
+});
+
+//静态资源托管
+app.use('/assets',express.static('assets'));
+
+//设置ejs
+app.set('view engine','ejs');
+//设置访问目录
+app.set('views','views');
+
